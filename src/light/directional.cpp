8018bd616c467d07843b775bf9af5508528343e9
ivarsrb <ivars.rb@gmail.com>
2019-09-13 08:24:54 +0300

    Add description

--- src/light/directional.cpp
+++ src/light/directional.cpp
@@ -4,7 +4,8 @@
 namespace light {
 Directional::Directional(const t::Vec3& direction, const t::Vec3& ambient_color, const t::Vec3& diffuse_color) :
     ICaster(ambient_color, diffuse_color, t::Vec3(0.0)),
-    direction_(glm::normalize(direction)* t::F32(-1.0)) {
+    // Reverse the vector
+    direction_(glm::normalize(direction) * t::F32(-1.0)) {
 }
 
 // Base diffuse color altered by light falling angle.
